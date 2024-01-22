1466871a215588fd3907e36acb0a8550460d20f9
Gokul230504 <gokulrajl.21ece@kongu.edu>
2024-01-22 14:43:06 +0530

    my 1st javascirpt file

--- new.js
+++ new.js
@@ -82,4 +82,17 @@ var a=10
 console.log(b)*///reference error
 //let changes locally if not let then value will be changed
 //hoisting
+//variable hoisiting
+console.log(a1)
+var a1=10//undefined
+//refernce error
+/*console.log(a2)
+let a2=10
+//Functional hoisiting*/
+newfun(1,2)
+function newfun(a3,b3)
+{
+    return a3+b3;
+}
+
 
