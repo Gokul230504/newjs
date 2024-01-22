c1d1c9ea083742b81168f0f3e7f17cafe3ccd488
Gokul230504 <gokulrajl.21ece@kongu.edu>
2024-01-22 14:27:41 +0530

    my new repo

--- new.js
+++ new.js
@@ -31,3 +31,55 @@ for (const iterator of NEWMAP1) {
     console.log(iterator);
 }
 console.log(NEWMAP1.has(5));
+set1.delete("3")
+console.log(set1)
+NEWMAP1.delete(1)
+console.log(NEWMAP1)
+function add(num1,num2)
+{
+    let num=num1+num2;
+    return num;
+}
+num1 = 1;
+num2 = 2;
+console.log("The sum of two numbers is",add(num1,num2))
+function num()
+{
+    let number=5;
+    return number;
+}
+console.log(num());
+//scope- globl,local,block
+//global scope
+Sum=0
+while(Sum<10)
+{
+   Sum++;
+}
+console.log(Sum);
+//local scope
+for(let i=0;i<10;i++)
+{
+    i++;
+    console.log(i);//locally declared so here it will print
+}
+//console.log(i);//wont print i;
+//block scope
+for(var i=0;i<10;i++)
+{
+    i++;
+    //console.log(i);//locally declared so here it will print
+}
+console.log(i);
+var a=10
+{
+    let b=5
+    let a=5
+    console.log(b)
+    console.log(a)
+}
+/*console.log(a)
+console.log(b)*///reference error
+//let changes locally if not let then value will be changed
+//hoisting
+
